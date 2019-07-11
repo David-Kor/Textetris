97e59aec38ec706a362c3bb9e705fcc6d3416900
David-Kor <36980670+David-Kor@users.noreply.github.com>
2019-07-12 02:10:11 +0900

    [190712] GameBoard 클래스 블록 회전,이동,전환 알고리즘 구현 완료 -> 보드판 적용 남음

--- Textris/GameBoard.cpp
+++ Textris/GameBoard.cpp
@@ -1,10 +1,384 @@
 #include "pch.h"
 #include "GameBoard.h"
 
+//새 블록을 생성하며 다음 블록을 컨트롤하게 함
+void GameBoard::CreateNextBlock()
+{
+	//다음 나올 블록을 현재 블록으로 지정
+	curBlk = nxtBlk;
+
+	if (curBlk != nullptr)
+	{
+		curBlk->posMainBlk.Y++;
+	}
+
+	//다음 나올 새 블록 생성
+	nxtBlk = new Blocks();
+	nxtBlk->rotateCnt = 0;
+	//메인블록 좌표 지정
+	nxtBlk->posMainBlk.X = MAX_HOR_SIZE / 2;
+	nxtBlk->posMainBlk.Y = 0;
+	nxtBlk->type = (BlockType)(rand() % 7);		//랜덤 타입 지정
+	UpdateBlock(nxtBlk);
+}
+
+//블록의 MainBlk 위치에 따라 SubBlk위치를 갱신. 실패 시 이전상태로 되돌아감. 성공여부 반환
+bool GameBoard::UpdateBlock(Blocks* blk)
+{
+	Blocks blkPreState = *blk;
+
+	//서브블록 좌표 지정
+	switch (blk->type)
+	{
+		//ㅣ자형 블록
+	case BlockType::I:
+		switch (blk->rotateCnt % 2)	//회전 가능 모양이 2개만 존재
+		{
+		case 0: {
+			//1번 블록
+			blk->posSubBlk1.X = blk->posMainBlk.X - 1;
+			blk->posSubBlk1.Y = blk->posMainBlk.Y;
+			//2번 블록
+			blk->posSubBlk2.X = blk->posMainBlk.X + 1;
+			blk->posSubBlk2.Y = blk->posMainBlk.Y;
+			//3번 블록
+			blk->posSubBlk3.X = blk->posMainBlk.X + 2;
+			blk->posSubBlk3.Y = blk->posMainBlk.Y;
+			break; }
+
+		case 1: {
+			//1번 블록
+			blk->posSubBlk1.X = blk->posMainBlk.X;
+			blk->posSubBlk1.Y = blk->posMainBlk.Y + 1;
+			//2번 블록
+			blk->posSubBlk2.X = blk->posMainBlk.X;
+			blk->posSubBlk2.Y = blk->posMainBlk.Y - 1;
+			//3번 블록
+			blk->posSubBlk3.X = blk->posMainBlk.X;
+			blk->posSubBlk3.Y = blk->posMainBlk.Y - 2;
+			break; }
+		}
+		break;
+
+		//ㅁ자형 블록
+	case BlockType::O:
+		blk->posSubBlk1.X = blk->posMainBlk.X + 1;
+		blk->posSubBlk1.Y = blk->posMainBlk.Y;
+		blk->posSubBlk2.X = blk->posMainBlk.X;
+		blk->posSubBlk2.Y = blk->posMainBlk.Y + 1;
+		blk->posSubBlk2.X = blk->posMainBlk.X + 1;
+		blk->posSubBlk2.Y = blk->posMainBlk.Y + 1;
+		break;
+
+		//ㅗ자형 블록
+	case BlockType::T:
+		switch (blk->rotateCnt)
+		{
+		case 0: {
+			//1번 블록
+			blk->posSubBlk1.X = blk->posMainBlk.X + 1;
+			blk->posSubBlk1.Y = blk->posMainBlk.Y;
+			//2번 블록
+			blk->posSubBlk2.X = blk->posMainBlk.X - 1;
+			blk->posSubBlk2.Y = blk->posMainBlk.Y;
+			//3번 블록
+			blk->posSubBlk3.X = blk->posMainBlk.X;
+			blk->posSubBlk3.Y = blk->posMainBlk.Y - 1;
+			break; }
+
+		case 1: {
+			//1번 블록
+			blk->posSubBlk1.X = blk->posMainBlk.X;
+			blk->posSubBlk1.Y = blk->posMainBlk.Y + 1;
+			//2번 블록
+			blk->posSubBlk2.X = blk->posMainBlk.X;
+			blk->posSubBlk2.Y = blk->posMainBlk.Y - 1;
+			//3번 블록
+			blk->posSubBlk3.X = blk->posMainBlk.X - 1;
+			blk->posSubBlk3.Y = blk->posMainBlk.Y;
+			break; }
+
+		case 2: {
+			//1번 블록
+			blk->posSubBlk1.X = blk->posMainBlk.X - 1;
+			blk->posSubBlk1.Y = blk->posMainBlk.Y;
+			//2번 블록
+			blk->posSubBlk2.X = blk->posMainBlk.X + 1;
+			blk->posSubBlk2.Y = blk->posMainBlk.Y;
+			//3번 블록
+			blk->posSubBlk3.X = blk->posMainBlk.X;
+			blk->posSubBlk3.Y = blk->posMainBlk.Y - 1;
+			break; }
+
+		case 3: {
+			//1번 블록
+			blk->posSubBlk1.X = blk->posMainBlk.X;
+			blk->posSubBlk1.Y = blk->posMainBlk.Y - 1;
+			//2번 블록
+			blk->posSubBlk2.X = blk->posMainBlk.X;
+			blk->posSubBlk2.Y = blk->posMainBlk.Y + 1;
+			//3번 블록
+			blk->posSubBlk3.X = blk->posMainBlk.X + 1;
+			blk->posSubBlk3.Y = blk->posMainBlk.Y;
+			break; }
+		}
+		break;
+
+		//ㄴ자형 블록
+	case BlockType::L:
+		switch (blk->rotateCnt)
+		{
+		case 0: {
+			//1번 블록
+			blk->posSubBlk1.X = blk->posMainBlk.X + 1;
+			blk->posSubBlk1.Y = blk->posMainBlk.Y;
+			//2번 블록
+			blk->posSubBlk2.X = blk->posMainBlk.X + 2;
+			blk->posSubBlk2.Y = blk->posMainBlk.Y;
+			//3번 블록
+			blk->posSubBlk3.X = blk->posMainBlk.X;
+			blk->posSubBlk3.Y = blk->posMainBlk.Y + 1;
+			break; }
+
+		case 1: {
+			//1번 블록
+			blk->posSubBlk1.X = blk->posMainBlk.X;
+			blk->posSubBlk1.Y = blk->posMainBlk.Y - 1;
+			//2번 블록
+			blk->posSubBlk2.X = blk->posMainBlk.X;
+			blk->posSubBlk2.Y = blk->posMainBlk.Y - 2;
+			//3번 블록
+			blk->posSubBlk3.X = blk->posMainBlk.X + 1;
+			blk->posSubBlk3.Y = blk->posMainBlk.Y;
+			break; }
+
+		case 2: {
+			//1번 블록
+			blk->posSubBlk1.X = blk->posMainBlk.X - 1;
+			blk->posSubBlk1.Y = blk->posMainBlk.Y;
+			//2번 블록
+			blk->posSubBlk2.X = blk->posMainBlk.X - 2;
+			blk->posSubBlk2.Y = blk->posMainBlk.Y;
+			//3번 블록
+			blk->posSubBlk3.X = blk->posMainBlk.X;
+			blk->posSubBlk3.Y = blk->posMainBlk.Y - 1;
+			break; }
+
+		case 3: {
+			//1번 블록
+			blk->posSubBlk1.X = blk->posMainBlk.X;
+			blk->posSubBlk1.Y = blk->posMainBlk.Y + 1;
+			//2번 블록
+			blk->posSubBlk2.X = blk->posMainBlk.X;
+			blk->posSubBlk2.Y = blk->posMainBlk.Y + 2;
+			//3번 블록
+			blk->posSubBlk3.X = blk->posMainBlk.X - 1;
+			blk->posSubBlk3.Y = blk->posMainBlk.Y;
+			break; }
+		}
+		break;
+
+		//ㄱ자형 블록
+	case BlockType::J:
+		switch (blk->rotateCnt)
+		{
+		case 0: {
+			//1번 블록
+			blk->posSubBlk1.X = blk->posMainBlk.X - 1;
+			blk->posSubBlk1.Y = blk->posMainBlk.Y;
+			//2번 블록
+			blk->posSubBlk2.X = blk->posMainBlk.X - 2;
+			blk->posSubBlk2.Y = blk->posMainBlk.Y;
+			//3번 블록
+			blk->posSubBlk3.X = blk->posMainBlk.X;
+			blk->posSubBlk3.Y = blk->posMainBlk.Y + 1;
+			break; }
+
+		case 1: {
+			//1번 블록
+			blk->posSubBlk1.X = blk->posMainBlk.X;
+			blk->posSubBlk1.Y = blk->posMainBlk.Y - 1;
+			//2번 블록
+			blk->posSubBlk2.X = blk->posMainBlk.X;
+			blk->posSubBlk2.Y = blk->posMainBlk.Y - 2;
+			//3번 블록
+			blk->posSubBlk3.X = blk->posMainBlk.X - 1;
+			blk->posSubBlk3.Y = blk->posMainBlk.Y;
+			break; }
+
+		case 2: {
+			//1번 블록
+			blk->posSubBlk1.X = blk->posMainBlk.X + 1;
+			blk->posSubBlk1.Y = blk->posMainBlk.Y;
+			//2번 블록
+			blk->posSubBlk2.X = blk->posMainBlk.X + 2;
+			blk->posSubBlk2.Y = blk->posMainBlk.Y;
+			//3번 블록
+			blk->posSubBlk3.X = blk->posMainBlk.X;
+			blk->posSubBlk3.Y = blk->posMainBlk.Y - 1;
+			break; }
+
+		case 3: {
+			//1번 블록
+			blk->posSubBlk1.X = blk->posMainBlk.X;
+			blk->posSubBlk1.Y = blk->posMainBlk.Y + 1;
+			//2번 블록
+			blk->posSubBlk2.X = blk->posMainBlk.X;
+			blk->posSubBlk2.Y = blk->posMainBlk.Y + 2;
+			//3번 블록
+			blk->posSubBlk3.X = blk->posMainBlk.X + 1;
+			blk->posSubBlk3.Y = blk->posMainBlk.Y;
+			break; }
+		}
+		break;
+
+		//ㄹ자형 블록
+	case BlockType::Z:
+		switch (blk->rotateCnt % 2)		//회전 가능 모양이 2개만 존재
+		{
+		case 0: {
+			//1번 블록
+			blk->posSubBlk1.X = blk->posMainBlk.X - 1;
+			blk->posSubBlk1.Y = blk->posMainBlk.Y;
+			//2번 블록
+			blk->posSubBlk2.X = blk->posMainBlk.X;
+			blk->posSubBlk2.Y = blk->posMainBlk.Y + 1;
+			//3번 블록
+			blk->posSubBlk3.X = blk->posMainBlk.X + 1;
+			blk->posSubBlk3.Y = blk->posMainBlk.Y + 1;
+			break; }
+
+		case 1: {
+			//1번 블록
+			blk->posSubBlk1.X = blk->posMainBlk.X;
+			blk->posSubBlk1.Y = blk->posMainBlk.Y + 1;
+			//2번 블록
+			blk->posSubBlk2.X = blk->posMainBlk.X + 1;
+			blk->posSubBlk2.Y = blk->posMainBlk.Y;
+			//3번 블록
+			blk->posSubBlk3.X = blk->posMainBlk.X + 1;
+			blk->posSubBlk3.Y = blk->posMainBlk.Y - 1;
+			break; }
+		}
+		break;
+
+		//5 자형 블록
+	case BlockType::S:
+		switch (blk->rotateCnt % 2)		//회전 가능 모양이 2개만 존재
+		{
+		case 0: {
+			//1번 블록
+			blk->posSubBlk1.X = blk->posMainBlk.X + 1;
+			blk->posSubBlk1.Y = blk->posMainBlk.Y;
+			//2번 블록
+			blk->posSubBlk2.X = blk->posMainBlk.X;
+			blk->posSubBlk2.Y = blk->posMainBlk.Y + 1;
+			//3번 블록
+			blk->posSubBlk3.X = blk->posMainBlk.X - 1;
+			blk->posSubBlk3.Y = blk->posMainBlk.Y + 1;
+			break; }
+
+		case 1: {
+			//1번 블록
+			blk->posSubBlk1.X = blk->posMainBlk.X;
+			blk->posSubBlk1.Y = blk->posMainBlk.Y + 1;
+			//2번 블록
+			blk->posSubBlk2.X = blk->posMainBlk.X - 1;
+			blk->posSubBlk2.Y = blk->posMainBlk.Y;
+			//3번 블록
+			blk->posSubBlk3.X = blk->posMainBlk.X - 1;
+			blk->posSubBlk3.Y = blk->posMainBlk.Y - 1;
+			break; }
+		}
+		break;
+	}
+
+	//배열을 벗어나는지 검사하여 취소함
+	//unsigned형이라 0보다 작을 수 없음(언더플로우로 최대값을 가지게 됨)
+	if (blk->posSubBlk1.X >= MAX_HOR_SIZE)
+	{
+		blk->posSubBlk1.X = blkPreState.posSubBlk1.X;
+		blk->posSubBlk1.Y = blkPreState.posSubBlk1.Y;
+		blk->posSubBlk2.X = blkPreState.posSubBlk2.X;
+		blk->posSubBlk2.Y = blkPreState.posSubBlk2.Y;
+		blk->posSubBlk3.X = blkPreState.posSubBlk3.X;
+		blk->posSubBlk3.Y = blkPreState.posSubBlk3.Y;
+		return false;
+	}
+	else if (blk->posSubBlk2.X >= MAX_HOR_SIZE)
+	{
+		blk->posSubBlk1.X = blkPreState.posSubBlk1.X;
+		blk->posSubBlk1.Y = blkPreState.posSubBlk1.Y;
+		blk->posSubBlk2.X = blkPreState.posSubBlk2.X;
+		blk->posSubBlk2.Y = blkPreState.posSubBlk2.Y;
+		blk->posSubBlk3.X = blkPreState.posSubBlk3.X;
+		blk->posSubBlk3.Y = blkPreState.posSubBlk3.Y;
+		return false;
+	}
+	else if (blk->posSubBlk3.X >= MAX_HOR_SIZE)
+	{
+		blk->posSubBlk1.X = blkPreState.posSubBlk1.X;
+		blk->posSubBlk1.Y = blkPreState.posSubBlk1.Y;
+		blk->posSubBlk2.X = blkPreState.posSubBlk2.X;
+		blk->posSubBlk2.Y = blkPreState.posSubBlk2.Y;
+		blk->posSubBlk3.X = blkPreState.posSubBlk3.X;
+		blk->posSubBlk3.Y = blkPreState.posSubBlk3.Y;
+		return false;
+	}
+	else if ((blk->posSubBlk1.Y >= MAX_VER_SIZE + MARGIN_BOARD)
+		&& blk->posMainBlk.Y > MARGIN_BOARD)	//보드판 아래 범위를 벗어날 경우에만(위쪽 범위 밖은 무시)
+	{
+		blk->posSubBlk1.X = blkPreState.posSubBlk1.X;
+		blk->posSubBlk1.Y = blkPreState.posSubBlk1.Y;
+		blk->posSubBlk2.X = blkPreState.posSubBlk2.X;
+		blk->posSubBlk2.Y = blkPreState.posSubBlk2.Y;
+		blk->posSubBlk3.X = blkPreState.posSubBlk3.X;
+		blk->posSubBlk3.Y = blkPreState.posSubBlk3.Y;
+		return false;
+	}
+
+	//곂치는 부분이 있으면 취소함
+	if (mv_ucBoardTable[blk->posSubBlk1.X][blk->posSubBlk1.Y] != 0)
+	{
+		blk->posSubBlk1.X = blkPreState.posSubBlk1.X;
+		blk->posSubBlk1.Y = blkPreState.posSubBlk1.Y;
+		blk->posSubBlk2.X = blkPreState.posSubBlk2.X;
+		blk->posSubBlk2.Y = blkPreState.posSubBlk2.Y;
+		blk->posSubBlk3.X = blkPreState.posSubBlk3.X;
+		blk->posSubBlk3.Y = blkPreState.posSubBlk3.Y;
+		return false;
+	}
+	else if (mv_ucBoardTable[blk->posSubBlk2.X][blk->posSubBlk2.Y] != 0)
+	{
+		blk->posSubBlk1.X = blkPreState.posSubBlk1.X;
+		blk->posSubBlk1.Y = blkPreState.posSubBlk1.Y;
+		blk->posSubBlk2.X = blkPreState.posSubBlk2.X;
+		blk->posSubBlk2.Y = blkPreState.posSubBlk2.Y;
+		blk->posSubBlk3.X = blkPreState.posSubBlk3.X;
+		blk->posSubBlk3.Y = blkPreState.posSubBlk3.Y;
+		return false;
+	}
+	else if (mv_ucBoardTable[blk->posSubBlk3.X][blk->posSubBlk3.Y] != 0)
+	{
+		blk->posSubBlk1.X = blkPreState.posSubBlk1.X;
+		blk->posSubBlk1.Y = blkPreState.posSubBlk1.Y;
+		blk->posSubBlk2.X = blkPreState.posSubBlk2.X;
+		blk->posSubBlk2.Y = blkPreState.posSubBlk2.Y;
+		blk->posSubBlk3.X = blkPreState.posSubBlk3.X;
+		blk->posSubBlk3.Y = blkPreState.posSubBlk3.Y;
+		return false;
+	}
+
+	//mv_ucBoardTable 변경할 것
+
+	return true;
+}
 
 GameBoard::GameBoard()
 {
 	int i, j;
+	//시드 변경
+	srand((unsigned int)time(NULL));
 
 	//게임 보드 초기화
 	for (i = 0; i < MAX_VER_SIZE; i++)
@@ -13,6 +387,7 @@ GameBoard::GameBoard()
 		for (j = 0; j < MAX_HOR_SIZE; j++)
 		{
 			mv_wstrBoard[i] += EMPTY;
+			mv_ucBoardTable[j][i] = 0;
 		}
 	}
 
@@ -36,11 +411,16 @@ GameBoard::GameBoard()
 			mv_wstrUI[i] += L"　점수　0";
 		}
 	}
+
+	curBlk = nullptr;
+	nxtBlk = nullptr;
 }
 
 
 GameBoard::~GameBoard()
 {
+	delete curBlk;
+	delete nxtBlk;
 }
 
 std::wstring* GameBoard::GetBoard()
@@ -51,4 +431,61 @@ std::wstring* GameBoard::GetBoard()
 std::wstring * GameBoard::GetUI()
 {
 	return mv_wstrUI;
+}
+
+//블록 회전
+void GameBoard::RotateBlock()
+{
+	curBlk->rotateCnt++;
+	if (UpdateBlock(curBlk) == false)
+	{
+		curBlk->rotateCnt--;
+	}
+}
+
+//방향이 양수면 오른쪽, 음수면 왼쪽, 0이면 아래 이동
+void GameBoard::MoveBlock(unsigned char ucDirection)
+{
+	if (ucDirection > 0)
+	{
+		//메인 블록이 우측 범위를 벗어나지 않는 경우
+		if (curBlk->posMainBlk.X + 1 < MAX_HOR_SIZE) {
+			curBlk->posMainBlk.X++;
+			if (UpdateBlock(curBlk) == false) {
+				curBlk->posMainBlk.X--;
+			}
+		}
+	}
+	else if (ucDirection < 0)
+	{
+		//메인 블록이 좌측 범위를 벗어나지 않는 경우
+		if (curBlk->posMainBlk.X - 1 >= 0) {
+			curBlk->posMainBlk.X--;
+			if (UpdateBlock(curBlk) == false) {
+				curBlk->posMainBlk.X++;
+			}
+		}
+	}
+	else
+	{
+		if (curBlk->posMainBlk.Y + 1 < MAX_VER_SIZE + MARGIN_BOARD) {
+			curBlk->posMainBlk.Y++;
+			//아래 이동이 실패하면 블록이 끝까지 이동한 것
+			if (UpdateBlock(curBlk) == false) {
+				curBlk->posMainBlk.Y--;
+				CreateNextBlock();		//새로운 블록을 생성해 현재 블록을 바꿈
+			}
+		}
+	}
+}
+
+//블록을 맨 밑까지 한번에 내림
+void GameBoard::DropBlock()
+{
+	while (UpdateBlock(curBlk))
+	{
+		curBlk->posMainBlk.Y++;
+	}
+	curBlk->posMainBlk.Y--;
+	CreateNextBlock();
 }
--- Textris/GameBoard.h
+++ Textris/GameBoard.h
@@ -4,27 +4,55 @@
 class GameBoard
 {
 private:
+	enum BoardType { BLOCK = L'■', EMPTY = L'　', WALL = L'▣' };
+	enum BlockType {I, O, T, L, J, Z, S };
+
+	struct Position
+	{
+		unsigned char X;
+		unsigned char Y;
+	};
+
+	struct Blocks
+	{
+		BlockType type;
+		unsigned char rotateCnt;
+		Position posMainBlk;
+		Position posSubBlk1;
+		Position posSubBlk2;
+		Position posSubBlk3;
+	};
+
 	int mv_nScore;
 
+	unsigned char mv_ucBoardTable[10][22];	//보드판 가로길이 x 보드판 세로길이+공백
 	std::wstring mv_wstrBoard[20];
 	std::wstring mv_wstrUI[23];
 
+	Blocks* curBlk;
+	Blocks* nxtBlk;
+
+	void CreateNextBlock();
+	bool UpdateBlock(Blocks* blk);
+
 public:
-	//보드판 세로길이, 가로길이, 전체 UI 길이
-	const short MAX_VER_SIZE = 20, MAX_HOR_SIZE = 10, MAX_UI_LINE = 23;
-	enum BoardType { BLOCK = L'■', EMPTY = L'　', WALL = L'▣' };
-	struct Blocks
-	{
-		unsigned char type;
-		unsigned char x[4];
-		unsigned char y[4];
-	};
+	//보드판 세로길이
+	const short MAX_VER_SIZE = 20;
+	//보드판 가로길이
+	const short MAX_HOR_SIZE = 10;
+	//전체 UI 라인 수
+	const short MAX_UI_LINE = 23;
+	//보이지 않는 공간
+	const short MARGIN_BOARD = 2;
 
 	GameBoard();
 	~GameBoard();
 
 	std::wstring* GetBoard();
 	std::wstring* GetUI();
+	void RotateBlock();
+	void MoveBlock(unsigned char ucDirection);
+	void DropBlock();
 };
 
 #endif
